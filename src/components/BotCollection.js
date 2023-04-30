e0dfb8c28ac6a7d246e8ed8a09e37751d5d702c2
QUANDEVV <vercelploy@gmail.com>
2023-04-30 19:58:36 +0300

    done

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -2,45 +2,45 @@ import React, { useState, useEffect } from 'react';
 import '../App.css';
 
 const BotCollection = ({ onBotEnlisted, onBotRelease }) => {
-  const [bots, setBots] = useState([]);
+const [bots, setBots] = useState([]);
 
-  useEffect(() => {
-    fetch('http://localhost:8001/bots')
-      .then(response => response.json())
-      .then(data => setBots(data))
-      .catch(error => console.error(error));
-  }, []);
+useEffect(() => {
+fetch('http://localhost:8001/bots')
+.then(response => response.json())
+.then(data => setBots(data))
+.catch(error => console.error(error));
+}, []);
 
-  useEffect(() => {
-    setBots(bots => bots.filter(bot => !onBotRelease || !onBotRelease(bot.id)));
-  }, [onBotRelease]);
+useEffect(() => {
+setBots(bots => bots.filter(bot => !onBotRelease || !onBotRelease(bot.id)));
+}, [onBotRelease]);
 
-  const handleEnlist = (bot) => {
-    onBotEnlisted(bot);
-  };
+const handleEnlist = (bot) => {
+onBotEnlisted(bot);
+};
 
-  return (
-    <div >
-      <h1>Bot Collection</h1>
-      <div className="card-row">
-        {bots.map(bot => (
-          <div className="card" key={bot.id}>
-            <img className="card-img-top" src={bot.avatar_url} alt={bot.name} />
-            <div className="card-body">
-              <h5 className="card-title">{bot.name}</h5>
-              <p className="card-text">Health: {bot.health}</p>
-              <p className="card-text">Damage: {bot.damage}</p>
-              <p className="card-text">Armor: {bot.armor}</p>
-              <p className="card-text">Class: {bot.bot_class}</p>
-              <p className="card-text">Catchphrase: {bot.catchphrase}</p>
+return (
+<div >
+<h1>Bot Collection</h1>
+<div className="card-row">
+{bots.map(bot => (
+<div className="card" key={bot.id}>
+<img className="card-img-top" src={bot.avatar_url} alt={bot.name} />
+<div className="card-body">
+<h5 className="card-title">{bot.name}</h5>
+<p className="card-text">Health: {bot.health}</p>
+<p className="card-text">Damage: {bot.damage}</p>
+<p className="card-text">Armor: {bot.armor}</p>
+<p className="card-text">Class: {bot.bot_class}</p>
+<p className="card-text">Catchphrase: {bot.catchphrase}</p>
 
-              <button onClick={() => handleEnlist(bot)}>Enlist</button>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+<button onClick={() => handleEnlist(bot)}>Enlist</button>
+</div>
+</div>
+))}
+</div>
+</div>
+);
 };
 
 export default BotCollection;
--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -2,42 +2,41 @@ import React from 'react';
 import '../App.css';
 
 const YourBotArmy = ({ army, releaseBot, dischargeBot }) => {
-  if (!army) return null;
+if (!army) return null;
 
-  const handleDischarge = (id) => {
-    fetch(`http://localhost:8001/bots/${id}`, {
-      method: 'DELETE',
-    })
-      .then(response => {
-        if (response.ok) {
-          dischargeBot(id);
-          alert(`Bot ${id} has been discharged forever!`);
-        }
-      })
-      .catch(error => console.error(error));
-  };
-  
+const handleDischarge = (id) => {
+fetch(`http://localhost:8001/bots/${id}`, {
+method: 'DELETE',
+})
+.then(response => {
+if (response.ok) {
+dischargeBot(id);
+alert(`Bot ${id} has been discharged forever!`);
+}
+})
+.catch(error => console.error(error));
+};
 
-  return (
-    <div className='your-bot-army yellow-bg'>
-      <h2>Your Bot Army</h2>
-      {army.map(bot => (
-        <div key={bot.id} className="card">
-          <img className="card-img-top" src={bot.avatar_url} alt={bot.name} />
-          <div className="card-body">
-            <h5 className="card-title">{bot.name}</h5>
-            <p className="card-text">Health: {bot.health}</p>
-            <p className="card-text">Damage: {bot.damage}</p>
-            <p className="card-text">Armor: {bot.armor}</p>
-            <p className="card-text">Class: {bot.bot_class}</p>
-            <p className="card-text">Catchphrase: {bot.catchphrase}</p>
-            <button onClick={() => releaseBot(bot)}>Remove</button>
-            <button className="discharge-btn" onClick={() => handleDischarge(bot.id)}>X</button>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+return (
+<div className='your-bot-army yellow-bg'>
+<h2>Your Bot Army</h2>
+{army.map(bot => (
+<div key={bot.id} className="card">
+<img className="card-img-top" src={bot.avatar_url} alt={bot.name} />
+<div className="card-body">
+<h5 className="card-title">{bot.name}</h5>
+<p className="card-text">Health: {bot.health}</p>
+<p className="card-text">Damage: {bot.damage}</p>
+<p className="card-text">Armor: {bot.armor}</p>
+<p className="card-text">Class: {bot.bot_class}</p>
+<p className="card-text">Catchphrase: {bot.catchphrase}</p>
+<button onClick={() => releaseBot(bot)}>Remove</button>
+<button className="discharge-btn" onClick={() => handleDischarge(bot.id)}>X</button>
+</div>
+</div>
+))}
+</div>
+);
 };
 
 export default YourBotArmy;
