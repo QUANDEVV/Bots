5a56241f633b081a824087af9ab02fd0ac8c96ec
QUANDEVV <vercelploy@gmail.com>
2023-05-02 08:43:00 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-import React, { useState } from 'react';
-import Collection from './components/BotCollection';
-import YourBotArmy from './components/YourBotArmy';
-import './App.css';
+      import React, { useState } from 'react';
+      import Collection from './components/BotCollection';
+      import YourBotArmy from './components/YourBotArmy';
+      import './App.css';
 
-const App = () => {
-const [army, setArmy] = useState([]);
+      const App = () => {
+      const [army, setArmy] = useState([]);
 
-const handleBotEnlisted = (bot) => {
-if (!army.find(b => b.id === bot.id)) {
-setArmy([...army, bot]);
-}
-};
+      const handleBotEnlisted = (bot) => {
+      if (!army.find(b => b.id === bot.id)) {
+      setArmy([...army, bot]);
+      }
+      };
 
-const handleBotRelease = (id) => {
-setArmy(army.filter(b => b.id !== id));
-};
+      const handleBotRelease = (id) => {
+      setArmy(army.filter(b => b.id !== id));
+      };
 
-const handleBotDischarge = (id) => {
-fetch(`https://my-json-server.typicode.com/QUANDEVV/bots/bots/${id}`, {
-method: 'DELETE',
-})
-.then(response => {
-if (response.ok) {
-setArmy(army.filter(b => b.id !== id)); // remove bot with specified id from army state directly
-}
-})
-.catch(error => console.error(error));
-};
+      const handleBotDischarge = (id) => {
+      fetch(`https://my-json-server.typicode.com/QUANDEVV/bots/bots/${id}`, {
+      method: 'DELETE',
+      })
+      .then(response => {
+      if (response.ok) {
+      setArmy(army.filter(b => b.id !== id)); // remove bot with specified id from army state directly
+      }
+      })
+      .catch(error => console.error(error));
+      };
 
-return (
-<div>
-<YourBotArmy army={army} releaseBot={handleBotRelease} dischargeBot={handleBotDischarge} setArmy={setArmy} />
+      return (
+        <div>
+      <YourBotArmy army={army} releaseBot={handleBotRelease} dischargeBot={handleBotDischarge} setArmy={setArmy} />
 
-<Collection onBotEnlisted={handleBotEnlisted} onBotRelease={handleBotRelease} />
-</div>
-);
-};
+      <Collection onBotEnlisted={handleBotEnlisted} onBotRelease={handleBotRelease} />
+      </div>
+      );
+      };
 
-export default App;
+      export default App;
